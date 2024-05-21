f0e834683a1c7bed2cc5c5d2ae1ebe510d803786
Anzu986 <den.ovsyankin@gmail.com>
2024-05-21 16:46:57 +0300

    улучшена авторизация

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,22 +3,9 @@ const bcrypt = require('bcryptjs')
 
 const login = (req, res) => {
     const { email, password } = req.body;
-  
+
     users
-      .findOne({ email }).then(user => {
-          if (!user) {
-            return Promise.reject(new Error("Неправильные почта или пароль"));
-          }
-      
-          return bcrypt.compare(password, user.password).then(matched => {
-            if (!matched) {
-            
-              return Promise.reject(new Error("Неправильные почта или пароль"));
-            }
-          
-            return user; 
-          });
-        })
+      .findUserByCredentials(email, password)
       .then((user) => {
         res
           .status(200)
